a28262166383cfcb70c987fc3626a08322eb5763
Dave Tallett <47983705+DaveTallett26@users.noreply.github.com>
2021-07-12 16:10:05 +0100

    to ringdirect

---
+++ geocities/onmouseover.js
@@ -0,0 +1,58 @@
+<HTML>
+<BODY BACKGROUND="grey.gif">
+<SCRIPT language='javascript'>
+<!--- hide javascript from old browsers;
+var navVersion = navigator.appVersion;
+var navName = navigator.appName;
+function windowHelp() {
+bubWindow=window.open('','HelpWindow','menubar=no,scrollbar=no,status=no,dependent=yes,screenX=-1,screenY=0,width=200,height=90,alwaysRaised=yes');
+bubWindow.document.write('');
+bubWindow.document.write('<BODY><HEAD><TITLE>Help window</TITLE></HEAD></BODY>');
+bubWindow.document.close();
+};
+function DispHelp(helpText){
+windowHelp();
+bubWindow.document.open();
+bubWindow.document.write('<B>'+helpText+'</B>');
+bubWindow.document.close();
+};
+function ClearHelp() {
+bubWindow.close();
+};
+// End script hiding from old browsers -->
+</SCRIPT>
+<A HREF="mw_sum1.html" TARGET='MAIN'
+onMouseOver="DispHelp('Development roles undertaken over the last eight years by the Principle Consultant.');  return true">
+<B>Summary</B></A>
+<P>
+<A HREF="mw_qual1.html" TARGET='MAIN'
+onMouseOver="DispHelp('Educational and professional qualifications of the Principle Consultant.'); return true">
+<B>Qualifications</B></A>
+<P>
+<A HREF="mw_trng1.html" TARGET="MAIN"
+onMouseOver="DispHelp('Formal I.T. training courses attended by the Principle Consultant.'); return true">
+<B>Training<B/></A>
+<P>
+<A HREF="mw_exp1.html" TARGET="MAIN"
+onMouseOver="DispHelp('Chronological experience record of the Principle Consultant with drill through to details.'); return true">
+<B>Experience<B/></A>
+<P>
+<A HREF="mw_ava1.html" TARGET="MAIN"
+onMouseOver="DispHelp('Availability of the Principle Consultant :- short and long term assignments.'); return true">
+<B>Availability<B/></A>
+<P>
+<A HREF="mw_assoc1.html" TARGET="MAIN"
+onMouseOver="DispHelp('Associate Consultants:- pen pictures and contact details.'); return true">
+<B>Associates<B/></A>
+<P>
+<A HREF="mw_main.html" TARGET="MAIN"
+onMouseOver="DispHelp('Return to the Introduction page originally displayed.'); return true">
+<B>Introduction<B/></A>
+<P>
+<HR>
+<P>
+<A HREF="mw_about1.html" TARGET="MAIN"
+onMouseOver="DispHelp('Technical details about this Web Site.'); return true">
+<B>About<B/></A>
+</BODY>
+
